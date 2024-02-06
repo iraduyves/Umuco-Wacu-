3e83fe5ee29867d05fee79ca1d0947c151ad48cc
Yves IRADUKUNDA <iradukundayves11@gmail.com>
2024-02-06 22:26:06 +0200

    fix:fixing some bugs

--- Screens/Home/Home.jsx
+++ Screens/Home/Home.jsx
@@ -44,7 +44,7 @@ export const Home = () => {
 
 
 
-hjhvbhjghjkhk
+hjhvbhjghjkh
 
           {/* <FontAwesomeIcon icon={faLocationDot} /> */}
           <Ionicons name='location-outline' size={24} />
