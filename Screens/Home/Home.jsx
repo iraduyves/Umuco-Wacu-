0689268d5ea73bae3e5088f1e02258d04cda4317
Yves IRADUKUNDA <iradukundayves11@gmail.com>
2024-02-07 20:29:20 +0200

    fix:fixing some bugs

--- Screens/Home/Home.jsx
+++ Screens/Home/Home.jsx
@@ -44,7 +44,7 @@ export const Home = () => {
           <Text style={Styles.location}>Rwanda</Text>
           <View style={{ alignItems: "flex-end" }}>
             <TouchableOpacity
-   dssjfgfdndkjbjkjk
+   dssjfgfdndkjbjkj
             >
               <Image source={require('../../assets/images/imagelogo.jpg')} style={{ width: 24, height: 24 }} />
             </TouchableOpacity>
